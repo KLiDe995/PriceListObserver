56c23c60b4e5d80dd7a626e1bd2dcec76cd2f923
Ivan Golovenkin <igolovenkin@yandex.ru>
2020-06-07 13:39:48 +0300

    Added module Adapter
    - added Adapter module

---
+++ Adapter/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module PriceListObserver.Adapter {
+    //exports ru.ivglv.PriceListAdapter.Adapter.Controller;
+    //exports ru.ivglv.PriceListAdapter.Adapter.Converter;
+    //exports ru.ivglv.PriceListAdapter.Adapter.Repository;
+}
