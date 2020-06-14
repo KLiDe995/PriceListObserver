80742aae7e5a9808d82a777514f9086b264cf481
Ivan Golovenkin <igolovenkin@yandex.ru>
2020-06-14 13:12:48 +0300

    Replaced exception classes from Domain to UseCase

--- Domain/src/main/java/module-info.java
+++ Domain/src/main/java/module-info.java
@@ -1,5 +1,4 @@
 module PriceListObserver.Domain {
     exports ru.ivglv.PriceListObserver.Model.Entity;
-    exports ru.ivglv.PriceListObserver.Model.Exceptions;
     exports ru.ivglv.PriceListObserver.Model.Port;
 }
